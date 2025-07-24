4f3dfd3e8d87167cfe21c7b93f4320720cabb65d
Seu Nome <alexkamp@gmail.com>
2025-07-24 08:12:21 -0300

    atualizacao cardapio
    Signed-off-by: Seu Nome <alexkamp@gmail.com>

--- modelos/avaliacao.py
+++ modelos/avaliacao.py
@@ -1,4 +1,8 @@
 class Avaliacao:
     def __init__(self, cliente, nota):
-        self._cliente = cliente
-        self._nota = nota
+        if not isinstance(nota, int):
+            raise TypeError("Nota deve ser um inteiro")
+        if not 1 <= nota <= 5:
+            raise ValueError("Nota deve estar entre 1 e 5")
+        self.cliente = cliente
+        self.nota = nota
